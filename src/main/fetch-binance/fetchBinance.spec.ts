782b0920048d900fcfe13d2362ca0781b48e570c
qirong77 <qirong7777@gmail.com>
2024-12-23 16:23:23 +0800

    test: 增加测试用例

--- src/main/fetch-binance/fetchBinance.spec.ts
+++ src/main/fetch-binance/fetchBinance.spec.ts
@@ -1,11 +1,11 @@
-import { it, expect, describe, test } from 'vitest' // 确保使用正确的导入
+import { it, expect, describe } from 'vitest'
 import { fetchContinuousKlines } from './fetchContinuousKlines'
 import { I_continuous_klines } from '../../common/types'
 import { fetchContinuousKlinesAllBySize } from './fetchContinuousKlinesAll'
 
 describe('fetch-binance', () => {
   it('fetchContinuousKlines', async () => {
-    const res = (await fetchContinuousKlines('ETC')) as I_continuous_klines[]
+    const res = (await fetchContinuousKlines('ETH')) as I_continuous_klines[]
     expect(res.length > 0).toBeTruthy()
     expect(res[0].low > 0).toBeTruthy()
     expect(res[0].high > 0).toBeTruthy()
@@ -13,5 +13,13 @@ describe('fetch-binance', () => {
   it('fetchContinuousKlinesAllBySize', async () => {
     const res1 = (await fetchContinuousKlinesAllBySize('SUI', 5, 1000 * 1)) as I_continuous_klines[]
     expect(res1.length === 1000).toBeTruthy()
+  })
+  it('fetchContinuousKlinesAllBySize重复率检测', async () => {
+    const klines = (await fetchContinuousKlinesAllBySize('SUI', 5, 1000 * 5)) as I_continuous_klines[]
+    const set = new Set()
+    klines.forEach((kline) => {
+      set.add(kline.start_time + '-' + kline.end_time)
+    })
+    expect(klines.length === set.size).toBeTruthy()
   })
 })
