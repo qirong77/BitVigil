1bbb6d5e4f0a20e82016809fbf334ee10605d8a3
qirong77 <qirong7777@gmail.com>
2024-12-24 16:16:45 +0800

    feat: 功能开发

--- src/common/coins/MAIN_COINS.ts
+++ src/common/coins/MAIN_COINS.ts
@@ -1,12 +1,12 @@
 export const MAIN_COINS = [
   'BTC',
-  // 'ETH',
-  // 'BNB',
-  // 'SOL',
-  // 'BCH',
-  // 'SUI',
-  // 'DOGE',
-  // '1000SHIB',
-  // '1000PEPE',
-  // '1000FLOKI'
+  'ETH',
+  'BNB',
+  'SOL',
+  'BCH',
+  'SUI',
+  'DOGE',
+  '1000SHIB',
+  '1000PEPE',
+  '1000FLOKI'
 ]
